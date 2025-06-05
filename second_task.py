1eebfee7152a4ebbe3b7031c9be80d5bc3e63d98
Mykhailo Rybkin <mykhailorybkin6@gmail.com>
2025-06-05 19:55:33 +0200

    Add 2 task

---
+++ second_task.py
@@ -0,0 +1,16 @@
+import random
+
+def get_number_ticket(min:int, max:int, quantity:int):
+    if quantity > (max - min):
+        return("Вибачте, помилка, кількість номерів більше за заданий діапазон")
+    elif max < min:
+        return("Вибачте, помилка, Ваше максимальне значення менше за мінімільне")
+    else:
+        numbers = list(range(min, max))
+        number_list = random.sample(numbers, quantity)
+        return number_list
+    
+        
+        
+
+print(get_number_ticket(1,100, 6))
