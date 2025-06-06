30e4e14769c9acf6f4bf08a3697777a6bc34425f
Mykhailo Rybkin <mykhailorybkin6@gmail.com>
2025-06-06 11:53:51 +0200

    First task corrected

--- first_task.py
+++ first_task.py
@@ -1,12 +1,14 @@
 import datetime
-date_now = datetime.datetime.today()
-print(date_now)
 
-def get_days_from_today(today, some_date):
-    difference = today - some_date
+
+
+def get_days_from_today(some_date):
+    date_now = datetime.datetime.today()
+    python_format_date = datetime.datetime.strptime(some_date, "%Y.%m.%d")
+    difference = date_now - python_format_date
     return difference.days
 
 some_date = "2026.10.10"
-python_format_date = datetime.datetime.strptime(some_date, "%Y.%m.%d")
 
-print(get_days_from_today(date_now, python_format_date), "days")
+
+print(get_days_from_today(some_date), "days")
