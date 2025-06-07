7d32b106930684e8b5cd7b0b9e5d4a8e5dae9893
Mykhailo Rybkin <mykhailorybkin6@gmail.com>
2025-06-07 19:23:49 +0200

    First task corrected

--- first_task.py
+++ first_task.py
@@ -1,14 +1,15 @@
 import datetime
 
-
-
 def get_days_from_today(some_date):
-    date_now = datetime.datetime.today()
-    python_format_date = datetime.datetime.strptime(some_date, "%Y.%m.%d")
-    difference = date_now - python_format_date
-    return difference.days
+    try:
+        date_now = datetime.datetime.today()
+        python_format_date = datetime.datetime.strptime(some_date, "%Y-%m-%d")
+        difference = date_now - python_format_date
+        return difference.days
+    except:
+        return "Incorrect type of data, please use YY-MM-DD format"
 
-some_date = "2026-10-10"
+some_date = "2026.10.10"
 
 
 print(get_days_from_today(some_date), "days")
